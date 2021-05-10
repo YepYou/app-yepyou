b1e1408e6e9161c16666add42ffee1f381670206
fernandovrgas <fvargaspf@gmail.com>
2021-05-10 15:40:34 -0300

    feat: :sparkles: Add enums to weeks and months

--- src/config.js
+++ src/config.js
@@ -1,6 +1,31 @@
 const config = {
     api: {
         url: 'http://api-yepyou-us.umbler.net/'
+    },
+    i18n: {
+        pt: {
+            weekDays: {
+                short: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab'],
+                long: ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']        
+            },
+            months: {
+                short: ['Jan', 'Fev', 'Mar', 'Abr', 'Maio', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+                long: [
+                    'Janeiro',
+                    'Fevereiro',
+                    'Março',
+                    'Abril',
+                    'Maio',
+                    'Junho',
+                    'Julho',
+                    'Agosto',
+                    'Setembro',
+                    'Outubro',
+                    'Novembro',
+                    'Dezembro'
+                ]
+            }
+        }
     }
 };
 
