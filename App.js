e30bef7672c3acfeb0cd882b16678f700b780396
fernandovrgas <fvargaspf@gmail.com>
2021-05-10 14:21:24 -0300

    feat: :sparkles: Add font family on project

--- App.js
+++ App.js
@@ -2,21 +2,51 @@ import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { Login } from './src/Login';
 import { default as theme } from './src/styles/theme.json';
-import { default as mapping } from './src/styles/mappding.json';
+import AppLoading from 'expo-app-loading';
 
-// import { AppNavigator } from './src/components';
-// <AppNavigator />
-const App = () => (
-    <>
-        <IconRegistry icons={EvaIconsPack}/>
-        <ApplicationProvider {...eva} 
-          theme={{ ...eva.light, ...theme }}
-          customMapping={mapping}>
-            <Login />
-        </ApplicationProvider>
-    </>
-);
+import {
+    useFonts,
+    OpenSans_300Light,
+    OpenSans_300Light_Italic,
+    OpenSans_400Regular,
+    OpenSans_400Regular_Italic,
+    OpenSans_600SemiBold,
+    OpenSans_600SemiBold_Italic,
+    OpenSans_700Bold,
+    OpenSans_700Bold_Italic,
+    OpenSans_800ExtraBold,
+    OpenSans_800ExtraBold_Italic,
+} from '@expo-google-fonts/open-sans';
+
+import { AppNavigator } from './src/components';
+
+const App = () => {
+    let [fontsLoaded] = useFonts({
+        OpenSans_300Light,
+        OpenSans_300Light_Italic,
+        OpenSans_400Regular,
+        OpenSans_400Regular_Italic,
+        OpenSans_600SemiBold,
+        OpenSans_600SemiBold_Italic,
+        OpenSans_700Bold,
+        OpenSans_700Bold_Italic,
+        OpenSans_800ExtraBold,
+        OpenSans_800ExtraBold_Italic
+    });
+
+    if (!fontsLoaded) {
+        return <AppLoading />;
+      } else {
+        return (
+            <>
+                <IconRegistry icons={EvaIconsPack} />
+                <ApplicationProvider {...eva} theme={{ ...eva.light, ...theme }} >
+                    <AppNavigator style={{ fontFamily: 'OpenSans_300Light' }}/>
+                </ApplicationProvider>
+            </>
+        );
+      }
+};
 
 export default App;
