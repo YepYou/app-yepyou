9a423a215f49765ef59da4f1e4e10b001185d624
fernandovrgas <fvargaspf@gmail.com>
2021-05-11 23:39:54 -0300

    feat: :sparkles: Add header layout screen

--- src/router/stacks/WorldStack.js
+++ src/router/stacks/WorldStack.js
@@ -1,14 +1,37 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import {
+    useTheme
+} from '@ui-kitten/components';
 
 import { World } from '../../screens';
+import colors from '../../styles/palette.json';
 
 function WorldStack() {
     const Stack = createStackNavigator();
+    const theme = useTheme();
 
     return (
-        <Stack.Navigator>
-            <Stack.Screen name="World" component={World} />
+        <Stack.Navigator 
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: theme['color-primary-500'],
+                    height: 140
+                },
+                headerTintColor: colors.headerText,
+                headerTitleStyle: {
+                    alignSelf: 'center',
+                    fontWeight: 'bold'
+                }
+            }}
+        >
+            <Stack.Screen
+                name="World" 
+                component={World}
+                options={{
+                    title: 'Mundos' 
+                }}
+            />
         </Stack.Navigator>
     );
 }
