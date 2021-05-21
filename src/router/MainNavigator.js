bbce68d849c8c6fb5751c51f0c32e2de289c2946
fernandovrgas <fvargaspf@gmail.com>
2021-05-21 10:32:19 -0300

    refactor: :rotating_light:

--- src/router/MainNavigator.js
+++ src/router/MainNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Image, StyleSheet } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { BottomNavigation, BottomNavigationTab, Icon, StyleService, useStyleSheet } from '@ui-kitten/components';
+import { BottomNavigation, BottomNavigationTab } from '@ui-kitten/components';
 
 import { WorldStack, MenuStack } from './stacks';
 
