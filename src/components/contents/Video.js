7c5b90535d38e61080ac0693d566f8662d3bdbb5
Gabriel Consalter <consaltergabriel@gmail.com>
2021-08-09 09:56:50 -0300

    fix: :bug: decreased video height removing bottom margin generated; linting

--- src/components/contents/Video.js
+++ src/components/contents/Video.js
@@ -3,20 +3,20 @@ import styled from 'styled-components';
 import YoutubeIframe from 'react-native-youtube-iframe';
 
 const Video = ({url}) => {
-	return (
-		<Container>
-			<YoutubeIframe
-				height={300}
-				videoId={url.split('v=')[1]}
-			/>
-		</Container>
-	);
-}
+  return (
+    <Container>
+      <YoutubeIframe
+        style={{backgroundColor: '#000'}}
+        height={200}
+        videoId={url.split('v=')[1]}
+      />
+    </Container>
+  );
+};
 
 const Container = styled.View`
-	width: 100%;
-	height: 300px;
-	margin: 8px 0;
+  width: 100%;
+  margin: 8px 0;
 `;
 
 export default Video;
