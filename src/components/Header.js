0b266ed69bfbfc04e0a22fb9201516a10554c1ae
Gabriel Consalter <consaltergabriel@gmail.com>
2021-08-09 10:12:44 -0300

    fix: :bug: fix header title text size and linting

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,68 +1,84 @@
 import React from 'react';
-import { Dimensions, Image, TouchableOpacity } from 'react-native';
-import { Layout, Text, StyleService, useStyleSheet } from '@ui-kitten/components';
-import { useNavigation } from '@react-navigation/native';
+import {Dimensions, Image, TouchableOpacity} from 'react-native';
+import {Layout, Text, StyleService, useStyleSheet} from '@ui-kitten/components';
+import {useNavigation} from '@react-navigation/native';
 
 import colors from '../styles/palette.json';
 import backButtom from '../../assets/backButtom.png';
-import { StageProgress } from '../components';
+import {StageProgress} from '../components';
 
-const Header = ({ title, goBack, translucent, stageProgress, onStageBack }) => {
-    const styles = useStyleSheet(themedStyles);
-    const navigation = useNavigation();
+const Header = ({title, goBack, translucent, stageProgress, onStageBack}) => {
+  const styles = useStyleSheet(themedStyles);
+  const navigation = useNavigation();
 
-    return (
-        <Layout style={[styles.container, {
-            position: translucent ? 'absolute' : 'relative',
-            backgroundColor: translucent ? 'transparent' : colors.secundary,
-            zIndex: translucent ? 999 : 0,
-            height: translucent ? 64 : 120,
-        }]}>
-            {goBack &&
-                <TouchableOpacity style={styles.backButtom} onPress={() => navigation.goBack()}>
-                    <Image style={styles.backButtomIcon} source={backButtom} />
-                </TouchableOpacity>
-            }
-            {onStageBack !== undefined &&
-                <TouchableOpacity style={styles.backButtom} onPress={() => onStageBack()}>
-                    <Image style={styles.backButtomIcon} source={backButtom} />
-                </TouchableOpacity>
-            }
-            {stageProgress !== undefined ? (
-                <StageProgress progress={stageProgress} />
-            ) : (
-                <Text style={styles.text} >
-                    {title}
-                </Text>
-            )}
-        </Layout>
-    );
+  return (
+    <Layout
+      style={[
+        styles.container,
+        {
+          position: translucent ? 'absolute' : 'relative',
+          backgroundColor: translucent ? 'transparent' : colors.secundary,
+          zIndex: translucent ? 999 : 0,
+          height: translucent ? 80 : 120,
+          justifyContent: goBack ? 'space-between' : 'center',
+        },
+      ]}>
+      {goBack && (
+        <TouchableOpacity
+          style={styles.backButtom}
+          onPress={() => navigation.goBack()}>
+          <Image style={styles.backButtomIcon} source={backButtom} />
+        </TouchableOpacity>
+      )}
+      {onStageBack !== undefined && (
+        <TouchableOpacity
+          style={styles.backButtom}
+          onPress={() => onStageBack()}>
+          <Image style={styles.backButtomIcon} source={backButtom} />
+        </TouchableOpacity>
+      )}
+      <Layout style={styles.contentBox}>
+        {stageProgress !== undefined ? (
+          <StageProgress progress={stageProgress} />
+        ) : (
+          <Text style={styles.text}>{title}</Text>
+        )}
+      </Layout>
+    </Layout>
+  );
 };
 
 const themedStyles = StyleService.create({
-    backButtom: {
-        alignSelf: 'flex-start',
-        left: 30,
-        top: Dimensions.get('window').height / 19
-    },
+  container: {
+    alignItems: 'center',
+    width: '100%',
+    top: 0,
+    flexDirection: 'row',
+  },
 
-    backButtomIcon: {
-        width: 30,
-        height: 20
-    },
-    
-	container: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: "100%",
-        top: 0,
-	},
+  backButtom: {
+    left: 20,
+  },
 
-    text: {
-        color: colors.headerText,
-        fontSize: 36,
-        fontWeight: 'bold',
-    }
+  backButtomIcon: {
+    width: 30,
+    height: 20,
+  },
+
+  contentBox: {
+    justifyContent: 'flex-start',
+    backgroundColor: 'transparent',
+    width: '100%',
+  },
+
+  text: {
+    color: colors.headerText,
+    fontSize: 36,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    alignSelf: 'center',
+  },
 });
 
 export default Header;
