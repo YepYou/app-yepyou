617872b1027ae71082e6dd5f5cf4bbf4a114cbf6
Gabriel Consalter <consaltergabriel@gmail.com>
2021-08-11 19:38:13 -0300

    feat: :sparkles: load missions with yepboards

--- src/screens/YepBoards.js
+++ src/screens/YepBoards.js
@@ -1,107 +1,126 @@
-import React from 'react';
-import { Dimensions } from 'react-native';
+import React, {useContext, useEffect, useState} from 'react';
+import {ActivityIndicator, Dimensions} from 'react-native';
 import styled from 'styled-components/native';
-import Balloon from "react-native-balloon";
+import Balloon from 'react-native-balloon';
 
 import Header from '../components/Header';
 import imageCharacter from '../../assets/peronsagem1.png';
 import plusIcon from '../../assets/plus.png';
+import api from '../services/api';
+import AuthContext from '../context/AuthContext';
+import {Loading} from '../components';
 
 const YepBoards = () => {
-	return (
-		<>
-			<Header goBack title="YepBoard" />
-			<Dialogue>
-				<Balloon
-					borderColor="#C3C3C3"
-					backgroundColor="#C3C3C3"
-					triangleDirection="right"
-					borderRadius={20}
-					triangleSize={10}
-					width={Dimensions.get('window').width * .6}>
-						<DialogueText>
-							Que tal conferir as anotações feitas em seu YepBoard?
-						</DialogueText>
-				</Balloon>
-				<CharacterImage
-					resizeMode="contain"
-					source={imageCharacter}
-				/>
-			</Dialogue>
-			<Container>
-				<YepBoard>
-					<YepBoardName>
-						Travelling Water
-					</YepBoardName>
-					<Plus>
-						<PlusIcon
-							resizeMode="contain"
-							source={plusIcon}/>
-					</Plus>
-				</YepBoard>
-			</Container>
-		</>
-	)
-}
+  const {user} = useContext(AuthContext);
+
+  const [loading, setLoading] = useState(true);
+  const [missions, setMissions] = useState([]);
+
+  useEffect(() => {
+    getYepBoardMissions();
+  }, []);
+
+  const getYepBoardMissions = async () => {
+    try {
+      const {data: missions} = await api.get(`/v1/yepboards?user=${user.id}`);
+      setMissions(missions.docs);
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const renderYepBoardMissions = () => {
+    return missions.map((mission) => (
+      <YepBoard>
+        <YepBoardName>{mission.name}</YepBoardName>
+        <Plus>
+          <PlusIcon resizeMode="contain" source={plusIcon} />
+        </Plus>
+      </YepBoard>
+    ));
+  };
+
+  return (
+    <>
+      <Header goBack itle="YepBoard" />
+      <Dialogue>
+        <Balloon
+          borderColor="#C3C3C3"
+          backgroundColor="#C3C3C3"
+          triangleDirection="right"
+          borderRadius={20}
+          triangleSize={10}
+          width={Dimensions.get('window').width * 0.6}>
+          <DialogueText>
+            Que tal conferir as anotações feitas em seu YepBoard?
+          </DialogueText>
+        </Balloon>
+        <CharacterImage resizeMode="contain" source={imageCharacter} />
+      </Dialogue>
+      <Container>{loading ? <Loading /> : renderYepBoardMissions()}</Container>
+    </>
+  );
+};
 
 const Container = styled.ScrollView`
-	flex: 1;
-	width: 100%;
-	padding: 0 16px;
-	margin-top: 86px;
+  flex: 1;
+  width: 100%;
+  padding: 0 16px;
+  margin-top: 86px;
 `;
 
 const Dialogue = styled.View`
-	width: 100%;
-	flex-direction: row;
-	position: absolute;
-	top: 70px;
-	right: 0;
-	align-items: center;
-	justify-content: flex-end;
+  width: 100%;
+  flex-direction: row;
+  position: absolute;
+  top: 70px;
+  right: 0;
+  align-items: center;
+  justify-content: flex-end;
 `;
 
 const DialogueText = styled.Text`
-	color: #706F6F;
-	font-weight: bold;
+  color: #706f6f;
+  font-weight: bold;
 `;
 
 const CharacterImage = styled.Image`
-	width: 120px;
-	height: 120px;
+  width: 120px;
+  height: 120px;
 `;
 
 const YepBoard = styled.TouchableOpacity`
-	width: 100%;
-	height: 60px;
-	background-color: #DFDFDF;
-	border-radius: 7px;
-	box-shadow: 2px 2px 3px rgba(0,0,0,0.2);
-	flex-direction: row;
-	align-items: center;
-	padding: 0 16px;
-	margin-bottom: 12px;
-	justify-content: space-between;
+  width: 100%;
+  height: 60px;
+  background-color: #dfdfdf;
+  border-radius: 7px;
+  box-shadow: 2px 2px 3px rgba(0, 0, 0, 0.2);
+  flex-direction: row;
+  align-items: center;
+  padding: 0 16px;
+  margin-bottom: 12px;
+  justify-content: space-between;
 `;
 
 const YepBoardName = styled.Text`
-	color: #706F6F;
-	font-weight: bold;
-	font-size: 25px;
+  color: #706f6f;
+  font-weight: bold;
+  font-size: 25px;
 `;
 
 const Plus = styled.View`
-	width: 30px;
-	height: 30px;
-	border-radius: 15px;
-	background-color: #706F6F;
-	align-items: center;
-	justify-content: center;
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background-color: #706f6f;
+  align-items: center;
+  justify-content: center;
 `;
 
 const PlusIcon = styled.Image`
-	width: 25px;
-	height: 25px;
+  width: 25px;
+  height: 25px;
 `;
 
 export default YepBoards;
