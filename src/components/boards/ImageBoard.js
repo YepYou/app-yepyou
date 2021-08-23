11b84a92d517633498a7281c24b24f49462a00d1
Gabriel Consalter <consaltergabriel@gmail.com>
2021-08-23 11:47:13 -0300

    feat: :sparkles: new image component to yepboard

---
+++ src/components/boards/ImageBoard.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+import {Modal} from 'react-native';
+import moment from 'moment';
+import styled from 'styled-components/native';
+
+const ImageBoard = ({url, date}) => {
+  const [fullscreen, setFullscreen] = useState(false);
+
+  return (
+    <>
+      <Container onPress={() => setFullscreen(true)}>
+        <Image
+          resizeMode="cover"
+          source={{
+            uri: url,
+          }}
+        />
+        <Footer>
+          <FooterText>
+            {`${date.type === 'creation' ? 'criado' : 'atualizado'} em ${moment(
+              date.date,
+            ).format('DD/MM/YY')}`}
+          </FooterText>
+        </Footer>
+      </Container>
+      <Modal visible={fullscreen} transparent={true}>
+        <Overlay onPress={() => setFullscreen(false)}>
+          <ImageFullscreen resizeMode="contain" source={{uri: url}} />
+        </Overlay>
+      </Modal>
+    </>
+  );
+};
+
+const Container = styled.TouchableOpacity`
+  flex: 1.35;
+  height: 180px;
+  border-radius: 21px;
+  margin: 4px;
+`;
+
+const Image = styled.Image`
+  width: 100%;
+  height: 100%;
+  border-radius: 21px;
+`;
+
+const ImageFullscreen = styled.Image`
+  width: 100%;
+  height: 100%;
+`;
+
+const Overlay = styled.TouchableOpacity`
+  background-color: #000000dd;
+`;
+
+const Footer = styled.View`
+  flex: 1;
+  padding: 16px;
+  position: absolute;
+  bottom: 0;
+`;
+
+const FooterText = styled.Text`
+  color: #fff;
+  font-size: 11px;
+  font-style: italic;
+`;
+
+export default ImageBoard;
--- src/components/boards/index.js
+++ src/components/boards/index.js
@@ -1,1 +1,2 @@
 export {default as TextBoard} from './TextBoard';
+export {default as ImageBoard} from './ImageBoard';
