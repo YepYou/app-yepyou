9815084754c4fff65b1adcb4f3a9f44dc3aef09f
Gabriel Consalter <consaltergabriel@gmail.com>
2021-09-02 17:16:00 -0300

    feat: :sparkles: added pdf viewer

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
-export { default as AppNavigator } from './AppNavigator';
-export { default as Loading } from './Loading';
-export { default as Header } from './Header';
-export { default as NoBadge } from './NoBadge';
-export { default as StageTitle } from './StageTitle';
-export { default as StageProgress } from './StageProgress';
-export { default as NoteCreator } from './NoteCreator';
+export {default as AppNavigator} from './AppNavigator';
+export {default as Loading} from './Loading';
+export {default as Header} from './Header';
+export {default as NoBadge} from './NoBadge';
+export {default as StageTitle} from './StageTitle';
+export {default as StageProgress} from './StageProgress';
+export {default as NoteCreator} from './NoteCreator';
+export {default as PDFScreen} from './PDFScreen';
