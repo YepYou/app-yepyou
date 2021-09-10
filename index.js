adb78db8bfa70ad9a4eafb633ce8b2e30e1bd621
Gabriel Consalter <consaltergabriel@gmail.com>
2021-09-10 16:35:54 -0300

    chore: :building_construction: update bundle version, safe are lib version, removed reactotron to release

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-import Reactotron from 'reactotron-react-native';
-import {NativeModules} from 'react-native';
-
-const {scriptURL} = NativeModules.SourceCode;
-const host = scriptURL.split('://')[1].split(':')[0];
-
-if (__DEV__) {
-  Reactotron.configure({
-    name: 'YepYou',
-    host,
-  })
-    .useReactNative({
-      networking: {
-        ignoreUrls: /symbolicate/,
-      },
-    })
-    .connect()
-    .clear();
-}
-
-console.tron = Reactotron;
-
 import {registerRootComponent} from 'expo';
 
 import App from './App';
